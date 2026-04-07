443924472e0f703dcdc9bfb29608485d6a8f773d
Yassine <yassinegomri2004@gmail.com>
2026-04-07 15:53:52 +0100

    resolve how data ordered in the dict, still improving

--- parser/parser.py
+++ parser/parser.py
@@ -1,39 +1,40 @@
 def parser(input_file):
     config_space = {}
     config_space["hubs"] = []
+    config_space["connections"] = []
     with open(input_file, "r") as file:
-        connections_count = 1
-        hub_counter = 1
         for line in file:
             if ":" not in line or line.startswith("#"):
                 continue
             line = line.strip()
-            # print(line)
             key, value = line.split(":")
             key = key.strip()
+            hub_cordinates = {}
+            connection_data = {}
             if "hub" in key:
-                hub_cordinates = {}
                 tmp = value.strip().split(" ")
-                # print(tmp)
                 hub_cordinates["name"] = tmp[0]
                 hub_cordinates["cordinates"] = [tmp[1], tmp[2]]
                 if len(tmp) > 2:
                     hub_cordinates["zone"] = {}
                     for data in tmp[3:]:
-                        sub_key, sub_value = data.split("=")
+                        sub_key, sub_value = data.split("=", 1)
                         sub_key = sub_key.replace("[", "")
                         sub_value = sub_value.replace("]", "")
                         hub_cordinates["zone"][sub_key] = sub_value
                 else:
-                    hub_cordinates["zone"] = {"color": tmp[3].split("=")[0][1:len(tmp[3]) - 1]}
+                    hub_cordinates["zone"] = {"color": tmp[3].split("=", 1)[0][1:len(tmp[3]) - 1]}
+                value = hub_cordinates
+            elif key == "connections":
+                tmp_key, tmp_value = value.split("-", 1)
+                connection_data[tmp_key] = {"from":tmp_key, "to":tmp_value}
+                value = connection_data
             if isinstance(value, dict):
-                if key in "hub":
+                if key == "hub":
                     config_space["hubs"].append(value)
-                if key in "connections":
+                if key == "connections":
                     config_space["connections"].append(value)
             else:
-                config_space[key] = value
-            # print(config_space)
                 
     print(config_space)
 
