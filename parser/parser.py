a0339ed0ba86f5024e940fe0037a6aabb25b4604
Yassine <yassinegomri2004@gmail.com>
2026-04-08 10:21:15 +0100

    extract the data from input file to dict

--- parser/parser.py
+++ parser/parser.py
@@ -4,39 +4,59 @@ def parser(input_file):
     config_space["connections"] = []
     with open(input_file, "r") as file:
         for line in file:
+            # overwrite comments and lines that does not have :
             if ":" not in line or line.startswith("#"):
                 continue
+            # split the line for two key and value from :
             line = line.strip()
             key, value = line.split(":")
             key = key.strip()
             hub_cordinates = {}
-            connection_data = {}
+            # get the hub data and set the zone informations in dict
             if "hub" in key:
                 tmp = value.strip().split(" ")
                 hub_cordinates["name"] = tmp[0]
                 hub_cordinates["cordinates"] = [tmp[1], tmp[2]]
-                if len(tmp) > 2:
-                    hub_cordinates["zone"] = {}
-                    for data in tmp[3:]:
-                        sub_key, sub_value = data.split("=", 1)
-                        sub_key = sub_key.replace("[", "")
-                        sub_value = sub_value.replace("]", "")
+                hub_cordinates["zone"] = {
+                    "type": None,
+                    "color": None,
+                    "max_drones": None
+                }
+                for data in tmp[3:]:
+                    sub_key, sub_value = data.split("=", 1)
+                    sub_key = sub_key.replace("[", "")
+                    sub_value = sub_value.replace("]", "")
+                    if sub_key == "zone":
+                        hub_cordinates["zone"]["type"] = sub_value
+                    else:   
                         hub_cordinates["zone"][sub_key] = sub_value
-                else:
-                    hub_cordinates["zone"] = {"color": tmp[3].split("=", 1)[0][1:len(tmp[3]) - 1]}
                 value = hub_cordinates
-            elif key == "connections":
+            # get the connectio data into and dict with from , to keys
+            elif key == "connection":
                 tmp_key, tmp_value = value.split("-", 1)
-                connection_data[tmp_key] = {"from":tmp_key, "to":tmp_value}
+                connection_data = {"from":tmp_key.strip(), "to":tmp_value.strip()}
                 value = connection_data
             if isinstance(value, dict):
                 if key == "hub":
                     config_space["hubs"].append(value)
-                if key == "connections":
+                if key == "connection":
                     config_space["connections"].append(value)
             else:
-                
-    print(config_space)
+                config_space[key] = value
+
+    for key, line in config_space.items():
+        if key == "hubs":
+            for dic in line:
+                print(dic)
+        elif key == "connections":
+            for con in line:
+                print(con)
+        else:
+            print(key, line)
+        print()
+    # print(config_space)
+        
+
 
 
 
